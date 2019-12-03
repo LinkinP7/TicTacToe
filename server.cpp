351af8117fe8dce637327e6ae33e431511f753ad
EC2 Default User <ec2-user@ip-172-31-23-160.us-east-2.compute.internal>
2019-12-03 15:38:43 +0000

    serverr

--- server.cpp
+++ server.cpp
@@ -49,7 +49,7 @@ int main()
 	
 	//size 받기 
 	clnt_addr_size=sizeof(clnt_addr);
-	recvfrom(serv_sock, &size,4, 0, &clnt_addr, &clnt_addr_size);
+	recvfrom(serv_sock, &size,4, 0,(struct sockaddr*) &clnt_addr, &clnt_addr_size);
 		    
    	 char buffer[bufsize];
 
@@ -72,68 +72,18 @@ int main()
     server_addr.sin_addr.s_addr = htons(INADDR_ANY);
     server_addr.sin_port = htons(portNum);
 
-    /* ---------- BINDING THE SOCKET ---------- */
-    /* ---------------- bind() ---------------- */
-
-
+  
     if ((bind(client, (struct sockaddr*)&server_addr,sizeof(server_addr))) < 0) 
     {
         cout << "=> Error binding connection, the socket has already been established..." << endl;
         return -1;
     }
 
-    /* 
-        The bind() system call binds a socket to an address, 
-        in this case the address of the current host and port number 
-        on which the server will run. It takes three arguments, 
-        the socket file descriptor. The second argument is a pointer 
-        to a structure of type sockaddr, this must be cast to
-        the correct type.
-    */
-
     size = sizeof(server_addr);
     cout << "=> Looking for clients..." << endl;
-
-    /* ------------- LISTENING CALL ------------- */
-    /* ---------------- listen() ---------------- */
-
     listen(client, 1);
-
-    /* 
-        The listen system call allows the process to listen 
-        on the socket for connections. 
-
-        The program will be stay idle here if there are no 
-        incomming connections.
-
-        The first argument is the socket file descriptor, 
-        and the second is the size for the number of clients 
-        i.e the number of connections that the server can 
-        handle while the process is handling a particular 
-        connection. The maximum size permitted by most 
-        systems is 5.
-
-    */
-
-    /* ------------- ACCEPTING CLIENTS  ------------- */
-    /* ----------------- listen() ------------------- */
-
-    /* 
-        The accept() system call causes the process to block 
-        until a client connects to the server. Thus, it wakes 
-        up the process when a connection from a client has been 
-        successfully established. It returns a new file descriptor, 
-        and all communication on this connection should be done 
-        using the new file descriptor. The second argument is a 
-        reference pointer to the address of the client on the other 
-        end of the connection, and the third argument is the size 
-        of this structure.
-    */
-
-    int clientCount = 1;
     server = accept(client,(struct sockaddr *)&server_addr,&size);
 
-    // first check if it is valid or not
     if (server < 0) 
         cout << "=> Error on accepting..." << endl;
 
@@ -143,19 +93,6 @@ int main()
         send(server, buffer, bufsize, 0);
         cout << "=> Connected with the client #" << clientCount << ", you are good to go..." << endl;
         cout << "\n=> Enter # to end the connection\n" << endl;
-
-        /* 
-            Note that we would only get to this point after a 
-            client has successfully connected to our server. 
-            This reads from the socket. Note that the read() 
-            will block until there is something for it to read 
-            in the socket, i.e. after the client has executed a 
-            the send().
-
-            It will read either the total number of characters 
-            in the socket or 1024
-        */
-
         cout << "Client: ";
         do {
             recv(server, buffer, bufsize, 0);
@@ -189,24 +126,7 @@ int main()
             } while (*buffer != '*');
         } while (!isExit);
 
-        /* 
-            Once a connection has been established, both ends 
-            can both read and write to the connection. Naturally, 
-            everything written by the client will be read by the 
-            server, and everything written by the server will be 
-            read by the client.
-        */
 
-        /* ---------------- CLOSE CALL ------------- */
-        /* ----------------- close() --------------- */
-
-        /* 
-            Once the server presses # to end the connection,
-            the loop will break and it will close the server 
-            socket connection and the client connection.
-        */
-
-        // inet_ntoa converts packet data to IP, which was taken from client
         cout << "\n\n=> Connection terminated with IP " << inet_ntoa(server_addr.sin_addr);
         close(server);
         cout << "\nGoodbye..." << endl;
@@ -216,4 +136,13 @@ int main()
 
     close(client);
     return 0;
+}
+
+void printboard(char **board,int size)
+{
+	for(int i=0;i<size;i++)
+		printf("---|");
+	for(int j=0;j<size;j++)
+		printf("---|");
+	printf("\n\n");
 }
